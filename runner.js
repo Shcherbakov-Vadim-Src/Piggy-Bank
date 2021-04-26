2bc45c20efc6a515ae3455dbd4efd4cb5f4b0225
vadim <79169082210@mail.ru>
2021-04-26 16:21:30 +0300

    Vadim1

--- runner.js
+++ runner.js
@@ -1,10 +1,20 @@
+const calculateTarget = document.querySelector('.calculate_target');
+const targetValue = document.querySelector('.target_value');
+const targetInput = document.querySelector('.target_input');
+const summInput = document.querySelector('.target_summ');
+const goalName = document.querySelector('.goal_name');
+const goalSumm = document.querySelector('.goal_summ'); // сумма накопения
+const goalTerm = document.querySelector('.goal_term'); // срок
+const startSumm = document.querySelector('.start_summ'); // стартовая сумма
+const percent = document.querySelector('.percent'); // процент
+const buttonShowSchem = document.querySelector('.show_schem'); // кнока рассчета
+const showSumm = document.querySelector('.replenishment_summ'); // сюда запишу результат
+const addGoal = document.querySelector('.goal_button');
+const plitka = document.querySelector('.plitka');
+const goalSetting = document.querySelector('.goal_setting');
+const deleteTar = document.querySelector('.deleteTar');
 const form = document.querySelector('form');
-const goalSumm = document.querySelector('.goal_summ');   // сумма накопения
-const goalTerm = document.querySelector('.goal_term');    // срок
-const startSumm = document.querySelector('.start_summ');  // стартовая сумма
-const percent = document.querySelector('.percent');      // процент
-const buttonShowSchem = document.querySelector('.show_schem');  // кнока рассчета
-const showSumm = document.querySelector('.replenishment_summ');  // сюда запишу результат
+let paymentSchedule = 0;
 
  
 form.addEventListener('submit', (event) => {
@@ -65,24 +75,6 @@ form.addEventListener('submit', (event) => {
 
     showSumm.innerText = (resultAcc / termYear).toFixed(2); // вывожу среднюю сумму пополнения
 });
-
-
-const calculateTarget = document.querySelector('.calculate_target');
-const targetValue = document.querySelector('.target_value');
-const targetInput = document.querySelector('.target_input');
-const summInput = document.querySelector('.target_summ');
-const goalName = document.querySelector('.goal_name');
-const goalSumm = document.querySelector('.goal_summ'); // сумма накопения
-const goalTerm = document.querySelector('.goal_term'); // срок
-const startSumm = document.querySelector('.start_summ'); // стартовая сумма
-const percent = document.querySelector('.percent'); // процент
-const buttonShowSchem = document.querySelector('.show_schem'); // кнока рассчета
-const showSumm = document.querySelector('.replenishment_summ'); // сюда запишу результат
-const addGoal = document.querySelector('.goal_button');
-const plitka = document.querySelector('.plitka');
-const goalSetting = document.querySelector('.goal_setting');
-const deleteTar = document.querySelector('.deleteTar');
-let paymentSchedule = 0;
 
 function addNewGoal() {
     let newPlitka = plitka.cloneNode(true);
